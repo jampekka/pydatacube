573fd4abe4f7c9a4790a2768f66a52329f75f644
Jami Pekkanen <jami.pekkanen@helsinki.fi>
2014-03-05 16:47:29 +0200

    Added setup.py and messed the directory structure :(
    Python module/import system kinda sucks

---
+++ setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup
+setup(name='Pydatacube',
+	version='0.0.0',
+	author='Jami Pekkanen',
+	author_email='jami.pekkanen@helsinki.fi',
+	url='https://github.com/jampekka/pydatacube',
+	packages=['pydatacube'],
+	)
