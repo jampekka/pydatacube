23577cf916bb36654b664854e2e25413652b22db
Jami Pekkanen <jami.pekkanen@helsinki.fi>
2014-03-05 18:48:30 +0200

    Sluging to pcaxis reader

--- pydatacube/pcaxis/__init__.py
+++ pydatacube/pcaxis/__init__.py
@@ -1,18 +1,50 @@
+# encoding: utf-8
 from collections import OrderedDict
+import string
 from ..pydatacube import _DataCube
 import px_reader
 
-def to_cube(pcaxis_data):
+default_translate = string.maketrans(
+	' -',
+	'__')
+
+class Sluger(object):
+	def __init__(self, translate=default_translate):
+		self.given_out = {}
+		self.translate = translate
+	
+	def __call__(self, value):
+		slug = value.encode('ascii', errors='ignore')
+		slug = slug.lower()
+		slug = slug.translate(self.translate)
+		chars = []
+		for c in slug:
+			if c == '_':
+				chars.append(c)
+			if c.isalnum():
+				chars.append(c)
+		slug = ''.join(chars)
+		realslug = slug
+		# Hopefully won't happen
+		while realslug in self.given_out:
+			realslug = realslug + '_'
+		return realslug
+
+def to_cube(pcaxis_data, Sluger=Sluger):
 	# TODO: Sluging of names for ids
 	px = px_reader.Px(pcaxis_data)
 	cube = OrderedDict()
 	dimensions = []
 	# Values is an ordered dict, so this
 	# should go fine.
+	dim_sluger = Sluger()
 	for label, px_categories in px.values.iteritems():
-		categories = [{'id': c} for c in px_categories]
+		cat_sluger = Sluger()
+		categories = [{'id': cat_sluger(c), 'label': c}
+			for c in px_categories]
 		dimension = dict(
-			id=label,
+			id=dim_sluger(label),
+			label=label,
 			categories=categories
 			)
 		dimensions.append(dimension)
@@ -24,7 +56,7 @@ def to_cube(pcaxis_data):
 	values = px._data.split()
 	
 	cube['value_dimensions'] = [
-		dict(id='value', values=values)
+		dict(id=dim_sluger('value'), values=values)
 		]
 
 	return _DataCube(cube)
